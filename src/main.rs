c3a741fb219a81c70666146383421a629efe158a
Nakaaaa <nakamura.master.12106033@gmail.com>
2023-07-26 20:16:54 +0900

    Add server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+    thread,
+};
+
+fn handle_client(mut stream: TcpStream) {
+    let response = "HTTP/1.1 200 OK\r\n\r\n\nHello, World!!!";
+
+    let _ = stream.write(response.as_bytes());
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:3000")?;
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(|| handle_client(stream));
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        }
+    }
+
+    Ok(())
 }
